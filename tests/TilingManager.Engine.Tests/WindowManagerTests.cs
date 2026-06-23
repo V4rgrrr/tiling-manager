73e944948c8c0e8dc80d4a2b0b65d29ca535cf34
Frugson <jakub.faruga@op.pl>
2026-06-23 18:05:14 +0200

    LaunchAppAndGetHandle and WindowManagerTests

---
+++ tests/TilingManager.Engine.Tests/WindowManagerTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace TilingManager.Engine.Tests;
+
+public class WindowManagerTests
+{
+    [Fact]
+    public void Should_Launch_Notepad_App_And_Return_Window_Handle()
+    {
+        // Arrange
+        var windowManager = new WindowManager();
+        var executablePath = @"C:\Windows\System32\notepad.exe";
+        
+        // Act
+        var windowHandle = windowManager.LaunchAppAndGetHandle(executablePath);
+        
+        // Assert
+        Assert.NotEqual(IntPtr.Zero, windowHandle);
+        
+        // Cleanup - not working ...
+        if (windowHandle != IntPtr.Zero)
+        {
+            Process processToKill = Process.GetProcessById((int)windowHandle);
+            
+            processToKill.Kill();
+            
+            processToKill.WaitForExit();
+        }
+    }
+}
